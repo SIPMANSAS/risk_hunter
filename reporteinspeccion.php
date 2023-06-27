79fb9de9a18b3ddd74edf519c1384ce2d7f533c7
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-06-27 17:15:07 -0500

    mejora

--- reporteinspeccion.php
+++ reporteinspeccion.php
@@ -1183,12 +1183,12 @@ $tabla .= "<style>
   }
 </style>";
 
-
+echo $tabla;
 // LIBRERIA DE DOMPDF
 
 use Dompdf\Dompdf;
 use Dompdf\Options;
-
+/*
 $options = new Options();
 $options->set([
     'isPhpEnabled' => true,
@@ -1261,4 +1261,4 @@ $dompdf->stream('ejemplo.pdf', array("Attachment" => false));
 unlink('MostrarPDF/Imagen_Grafica.png');
 unlink('MostrarPDF/Imagen_Grafica2.png');
 unlink('MostrarPDF/mapa.png');
-/* */
+ */
