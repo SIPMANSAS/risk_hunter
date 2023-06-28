f586c2689e0502ac280a79c27d2819d8f4dd6f63
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-06-28 10:28:12 -0500

    a

--- reporteinspeccion.php
+++ reporteinspeccion.php
@@ -1178,11 +1178,11 @@ if ($origen == '1') {
     }
     $tabla .= "</table>";
 }
-//echo $tabla;
+echo $tabla;
 /////////////////////////// END PINTA LOS TITULOS MAS EXTERNOS DE LA MATRIZ ARRIBA /////////////////////////
 
 // LIBRERIA DE DOMPDF
-
+/*
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
@@ -1258,4 +1258,4 @@ $dompdf->stream('ejemplo.pdf', array("Attachment" => false));
 unlink('MostrarPDF/Imagen_Grafica.png');
 unlink('MostrarPDF/Imagen_Grafica2.png');
 unlink('MostrarPDF/mapa.png');
-/* */
+ */
