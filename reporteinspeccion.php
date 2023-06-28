1f66595ab22273b9f013aba238dace4c585e312a
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-06-28 12:12:09 -0500

    a

--- reporteinspeccion.php
+++ reporteinspeccion.php
@@ -341,7 +341,7 @@ if ($origen == '1') {
     $tabla .= "";
     /////////////////////////// END PINTA LOS TITULOS MAS EXTERNOS DE LA MATRIZ ARRIBA /////////////////////////
     $consultaFilas = $mysqli->query("SELECT D.nombre,D.identificador ,COUNT(D.identificador) AS Cantidad FROM mat_filas C ,cg_valores_dominio D WHERE C.vdom_calificacion = D.identificador GROUP BY D.identificador ORDER BY `D`.`identificador` DESC");
-    $tabla .= "   <td class='verticalText' style='background-color:#00E0FF;width: 50px;' colspan='1' rowspan='$textoC'>
+    $tabla .= "   <td class='verticalText' style='background-color:#00E0FF;width: 50px;' colspan='1' rowspan=$textoC>
                 <h3>
                     <center>Probabilidad(%)</center>
                 </h3>
