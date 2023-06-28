a5242dcaca2d9fedeb1f72aa34f6cd0222a06947
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-06-28 12:29:02 -0500

    a

--- reporteinspeccion.php
+++ reporteinspeccion.php
@@ -357,7 +357,7 @@ if ($origen == '1') {
         $extraermatcolumnas['Cantidad'] . '-' . $extraermatcolumnas['nombre'];
         $ancho = $extraermatcolumnas['Cantidad'] . "%";
         $color = $colores[$num - 1];
-        $tabla .= "<td colspan='6' class='verticalTextB' style='background-color: $color;height: $ancho;border: 1px solid black;'><b>" . $extraermatcolumnas['nombre'] . "<b></td></tr>";
+        $tabla .= "<td colspan='6' class='verticalTextB' style='background-color: $color;height: $ancho;border: 1px solid black;'><b>" . $extraermatcolumnas['nombre'] . "<b></td><tr>";
     }
    // $tabla .= "";
 
